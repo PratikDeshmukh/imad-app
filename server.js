2f12412e7b880b840d90ea5369f7afb613c2686d
PratikDeshmukh <36589499+PratikDeshmukh@users.noreply.github.com>
2018-02-20 17:44:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,49 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    var date = data.date;
+    var htmlTemplate = `
+    <html>
+        <head>
+        <title> 
+         ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+        
+        </head>
+        
+        
+        <body>
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+              ${heading}  
+            </h3>
+            <hr/>
+            
+            <div>
+                ${date} 
+            </div>    
+           <div class="container"> 
+                <div>
+                    ${content}
+                </div>
+             </div>  
+        </body>    
+        
+    </html>
+    
+    
+    
+    `;
+    return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
