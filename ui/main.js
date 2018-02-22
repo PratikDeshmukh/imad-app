3a49c05c95593564458335e79f23fd9f60a5895b
PratikDeshmukh <36589499+PratikDeshmukh@users.noreply.github.com>
2018-02-22 16:21:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,23 @@
 console.log('Loaded!');
 
 var button = document.getElementById('counter');
-var counter = 0;
+//var counter = 0;
 
 button.onclick = function(){
-    counter = counter +1;
+   // counter = counter +1;
+    var request = new XMLHttpRequest();
     
-    var span = document.getElementById('count')
-    span.innerHTML=counter.toString();  
+    request.onreadystatechange = function(){
+        if (request.readystate == XMLHttpRequest.DONE){
+            if(request.status == 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString();              
+            }
+        }
+    };
+    
+    request.open('GET','http://pratikdeshmukh13-imad.hasura-app.io/counter',true);
+    request.send(null);
+    
 };
