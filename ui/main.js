198000d96764c9ae5a1640716e4a05d46dc9eebc
PratikDeshmukh <36589499+PratikDeshmukh@users.noreply.github.com>
2018-02-22 11:57:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,13 @@ var element = document.getElementById('main-text');
 element.innerHTML="NEW VALUE";
 
 var img = document.getElementById('madi');
+var maginLeft = 0;
+
+function moveRight(){
+   marginLeft = marginLeft + 10;
+   img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick = function(){
     img.style.marginLeft = "100px";
 };
