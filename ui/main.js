a986545624c2747e14bb0e05c9d19120701aca7f
PratikDeshmukh <36589499+PratikDeshmukh@users.noreply.github.com>
2018-02-22 16:23:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,7 @@ button.onclick = function(){
         }
     };
     
-    request.open('GET','http://pratikdeshmukh13-imad.hasura-app.io/counter',true);
+    request.open('GET','http://pratikdeshmukh13.imad.hasura-app.io/counter',true);
     request.send(null);
     
 };
