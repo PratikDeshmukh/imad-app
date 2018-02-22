3f8d607049327777fc6394e46608e833b73966f7
PratikDeshmukh <36589499+PratikDeshmukh@users.noreply.github.com>
2018-02-22 11:49:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,3 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML="NEW VALUE";
-
-var img = document.getElementById('madi');
-img.onClick = fuction(){
-img.style.marginLeft="100px";
-}
