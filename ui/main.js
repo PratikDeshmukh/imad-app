738f9424ddb3bb050431f7458cc70727a88c841d
PratikDeshmukh <36589499+PratikDeshmukh@users.noreply.github.com>
2018-02-23 22:09:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,6 +34,7 @@ submit.onclick = function(){
         if (request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
                 var names = reqeust.responseText;
+                names = JSON.parse(names);
                 var list = '';
     
                 for(var i=0; i< names.length; i++){
