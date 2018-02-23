3e40faa7f56bd27d8c869bc7638740e2cca693d6
PratikDeshmukh <36589499+PratikDeshmukh@users.noreply.github.com>
2018-02-23 22:03:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,15 @@ app.get('/counter', function( req, res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
+var names=[];
+app.get('/submit-name', function( req, res) {
+    
+    var name = req.query.name;
+    names.push(name);
+    
+    res.send(JSON.stringify(names));
+});
+
 
 app.get('/article-two', function( req, res) {
     res.sendFile(path.join(__dirname,'article-two.html'));
