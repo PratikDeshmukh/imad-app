f4fcb9e1609d37e889ca04f603b51939bd39d1ea
PratikDeshmukh <36589499+PratikDeshmukh@users.noreply.github.com>
2018-02-23 21:43:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,21 @@ button.onclick = function(){
     request.send(null);
     
 };
+
+//submit names
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    
+    var names = ['Pratik','Madhura','Kamal'];
+    var list = '';
+    
+    for(var i=0; i< names.length; i++){
+        list += '<li>'+ names[i] + '</li>';
+    }
+    
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
+
