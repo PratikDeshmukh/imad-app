dccc7a87d735d0ca880f9312bea6e3bf5d1c471b
PratikDeshmukh <36589499+PratikDeshmukh@users.noreply.github.com>
2018-02-27 21:43:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
 var app = express();
 app.use(morgan('combined'));
 
+var config = {
+    user:'pratikdeshmukh13',
+    database:'pratikdeshmukh13',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
 
 var articleOne = {
   title:'Article One',
@@ -66,6 +73,17 @@ app.get('/', function (req, res) {
 
 app.get('/article-one', function( req, res) {
     res.send(createTemplate(articleOne));
+});
+var pool = new Pool(config);
+app.get('/test-db', function( req, res) {
+    
+    pool.query('select * article',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    })
 });
 
 var counter = 0;
