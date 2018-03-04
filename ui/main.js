2057e911fdb56dbecc48e7f12673050a76612b54
PratikDeshmukh <36589499+PratikDeshmukh@users.noreply.github.com>
2018-03-04 10:10:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -79,7 +79,7 @@ login.onclick = function(){
     //console.log(username);
     //console.log(password);
 
-    request.open('GET','http://pratikdeshmukh13.imad.hasura-app.io/create-user',true);
+    request.open('POST','http://pratikdeshmukh13.imad.hasura-app.io/create-user',true);
     request.setRequestHeader('Content-type','application/json');
     request.send(JSON.stringify({username: username, password: password}));
     
