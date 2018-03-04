4855b2ef804182626ceb972709b42178c376696b
PratikDeshmukh <36589499+PratikDeshmukh@users.noreply.github.com>
2018-03-04 10:00:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,3 +57,34 @@ submit.onclick = function(){
     
 };
 
+var login = document.getElementById('login_btn');
+login.onclick = function(){
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                console.log('user logged in');
+                alert('login successful');
+            }else if(request.status === 403){
+                alert('username/password incorrect');
+            }else if(request.status === 500){
+                alert('something went wrong on server');
+            }
+        }
+    };
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+
+    request.open('GET','http://pratikdeshmukh13.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-type','application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+    
+    
+    
+};
+
+
